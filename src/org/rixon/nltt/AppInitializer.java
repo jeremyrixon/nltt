4a2ba111edec9af0c45cd2ef280c1c7f54a438d2
Jeremy Rixon <jrixon@deploypartners.com>
2016-02-24 18:14:24 +1100

    Hello World.

---
+++ src/org/rixon/nltt/AppInitializer.java
@@ -0,0 +1,35 @@
+package org.rixon.nltt;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+ 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import java.util.logging.Logger;
+ 
+public class AppInitializer implements WebApplicationInitializer {
+ 
+    private static final String CONFIG_LOCATION = "org.rixon.nltt";
+    private final static Logger log = Logger.getLogger(AppInitializer.class.getName()); 
+ 
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+ 
+        log.info("Initializing Application for " + servletContext.getServerInfo());
+ 
+        // Create ApplicationContext
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.setConfigLocation(CONFIG_LOCATION);
+ 
+        // Add the servlet mapping manually and make it initialize automatically
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("mvc-dispatcher", dispatcherServlet);
+ 
+        servlet.addMapping("/");
+        servlet.setAsyncSupported(true);
+        servlet.setLoadOnStartup(1);
+    }
+}
---
+++ src/org/rixon/nltt/controller/AppController.java
@@ -0,0 +1,15 @@
+package org.rixon.nltt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+@Controller
+public class AppController {
+    @ResponseBody
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
